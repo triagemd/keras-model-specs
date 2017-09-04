dab3a131b2bd5b7d70ce89ca4a8ec0487bef2d19
Steven Normore <snormore@gmail.com>
2017-09-04 15:25:46 -0400

    Add MANIFEST.in

--- setup.py
+++ setup.py
@@ -5,14 +5,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='keras-model-specs',
-    version='0.0.1',
+    version='0.0.4',
     description='A helper package for managing Keras model base architectures with overrides for target size and preprocessing functions.',
     author='Triage Technologies Inc.',
     author_email='ai@triage.com',
     url='https://www.triage.com/',
     packages=find_packages(exclude=['tests', '.cache', '.venv', '.git', 'dist']),
-    scripts=[
-    ],
+    include_package_data=True,
     install_requires=[
         'Keras',
         'Pillow',
