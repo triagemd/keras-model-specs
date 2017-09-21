c4a1dd96b03aac5b9bc780468c31e455d127660c
Steven Normore <snormore@gmail.com>
2017-09-21 14:01:12 -0400

    Add license header to resnet152 with reference to cnn_finetune

--- keras_model_specs/models/resnet152.py
+++ keras_model_specs/models/resnet152.py
@@ -1,6 +1,29 @@
-# ResNet-152 in Keras was posted by flyyufelix
-# https://gist.github.com/flyyufelix/7e2eafb149f72f4d38dd661882c554a6
-# without a license.
+# MIT License
+
+# This has been modified and modifications are
+# Copyright 2017 Triage Technologies Inc.
+
+# Copyright (c) 2017 Felix Yu
+# https://github.com/flyyufelix/cnn_finetune/blob/master/resnet_152.py
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 
 import sys
 import warnings
