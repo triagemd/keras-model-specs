619f44ebd060582b518f2c12c46a4384b0205944
Albert Jimenez <albertjimenezsanfiz@gmail.com>
2018-06-15 12:34:41 -0400

    Update Keras version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='keras-model-specs',
-    version='0.0.25',
+    version='0.0.26',
     description='A helper package for managing Keras model base architectures with overrides for target size and preprocessing functions.',
     author='Triage Technologies Inc.',
     author_email='ai@triage.com',
@@ -13,7 +13,7 @@ setup(
     packages=find_packages(exclude=['tests', '.cache', '.venv', '.git', 'dist']),
     include_package_data=True,
     install_requires=[
-        'Keras>=2.1.6',
+        'Keras>=2.2.0',
         'h5py',
         'Pillow',
         'six'
