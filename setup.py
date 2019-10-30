1cc1891303e90e0d38d85adf89bf7225d0d1517d
Albert Jimenez <albertjimenezsanfiz@gmail.com>
2019-10-30 14:05:24 -0400

    Remove keras from setup.py

--- setup.py
+++ setup.py
@@ -5,16 +5,16 @@ from setuptools import setup, find_packages
 
 setup(
     name='keras-model-specs',
-    version='1.2.0',
-    description='A helper package for managing Keras model base architectures with overrides for target size and preprocessing functions.',
+    version='2.0.0',
+    description='A helper package for managing tf.keras model base architectures with overrides for target size '
+                'and preprocessing functions.',
     author='Triage Technologies Inc.',
     author_email='ai@triage.com',
     url='https://www.triage.com/',
     packages=find_packages(exclude=['tests', '.cache', '.venv', '.git', 'dist']),
     include_package_data=True,
     install_requires=[
-        'Keras',
-        'Keras-Applications>=1.0.7',
+        'tensorflow>=2.0',
         'h5py',
         'Pillow',
         'six'
