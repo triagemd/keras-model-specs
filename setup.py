6ebffbe30167c0e20c436bc3272897cc90f4a326
Albert Jimenez <albertjimenezsanfiz@gmail.com>
2020-04-16 11:01:17 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='keras-model-specs',
-    version='2.0.1',
-    description='A helper package for managing tf.keras model base architectures with overrides for target size '
-                'and preprocessing functions.',
+    version='2.1.0',
+    description='A helper package for managing keras model base architectures (from keras_applications) '
+                'with overrides for target size and preprocessing functions.',
     author='Triage Technologies Inc.',
     author_email='ai@triage.com',
     url='https://www.triage.com/',
